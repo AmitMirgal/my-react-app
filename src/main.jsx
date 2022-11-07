bdf4b6dbe87236f81f97f0f4d7124a61b43569ad
amirgal <amit.s.mirgal@gmail.com>
2022-11-06 19:14:46 -0500

    react router implementation

--- src/main.jsx
+++ src/main.jsx
@@ -4,30 +4,76 @@ import Root, {
 	loader as rootLoader,
 	action as rootAction,
 } from "./routes/root";
+import {
+	loader as contactLoader,
+	action as contactAction,
+} from "./routes/contact";
+import { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+
+const LazyErrorPage = React.lazy(() => import("./error-page"));
+const LazyEditContact = React.lazy(() => import("./routes/edit"));
+const LazyContact = React.lazy(() => import("./routes/contact"));
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Root />,
-		errorElement: <ErrorPage />,
+		errorElement: (
+			<React.Suspense fallback={<>fallback component</>}>
+				<LazyErrorPage />
+			</React.Suspense>
+		),
 		loader: rootLoader,
 		action: rootAction,
 		children: [
 			{
-				path: "contacts/:contactId",
-				element: <Contact />,
+				errorElement: (
+					<React.Suspense fallback={<>fallback component</>}>
+						<LazyErrorPage />
+					</React.Suspense>
+				),
+				children: [
+					{ index: true, element: <Index /> },
+					{
+						path: "contacts/:contactId",
+						element: (
+							<React.Suspense fallback={<>fallback component</>}>
+								<LazyContact />
+							</React.Suspense>
+						),
+						loader: contactLoader,
+						action: contactAction,
+					},
+					{
+						path: "contacts/:contactId/edit",
+						element: (
+							<React.Suspense fallback={<>fallback component</>}>
+								<LazyEditContact />
+							</React.Suspense>
+						),
+						loader: contactLoader,
+						action: editAction,
+					},
+					{
+						path: "contacts/:contactId/destroy",
+						action: destroyAction,
+						errorElement: <div>Oops! There was an error.</div>,
+					},
+				],
 			},
 		],
 	},
 ]);
 
+// here is the doc for code-splitting technique with react router
+// https://dev.to/infoxicator/data-routers-code-splitting-23no
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,14 +1,19 @@
-import { Form } from "react-router-dom";
+import { Form, useLoaderData, useFetcher } from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
+
+export async function action({ request, params }) {
+	let formData = await request.formData();
+	return updateContact(params.contactId, {
+		favorite: formData.get("favorite") === "true",
+	});
+}
+
+export async function loader({ params }) {
+	return getContact(params.contactId);
+}
 
 export default function Contact() {
-	const contact = {
-		first: "Your",
-		last: "Name",
-		avatar: "https://placekitten.com/g/200/200",
-		twitter: "your_handle",
-		notes: "Some notes",
-		favorite: true,
-	};
+	const contact = useLoaderData();
 
 	return (
 		<div id="contact">
@@ -60,10 +65,15 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
-	// yes, this is a `let` for later
+	const fetcher = useFetcher();
 	let favorite = contact.favorite;
+
+	if (fetcher.formData) {
+		favorite = fetcher.formData.get("favorite") === "true";
+	}
+
 	return (
-		<Form method="post">
+		<fetcher.Form method="post">
 			<button
 				name="favorite"
 				value={favorite ? "false" : "true"}
@@ -71,6 +81,6 @@ function Favorite({ contact }) {
 			>
 				{favorite ? "★" : "☆"}
 			</button>
-		</Form>
+		</fetcher.Form>
 	);
 }
